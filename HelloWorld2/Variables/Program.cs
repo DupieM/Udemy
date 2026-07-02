64a1f465b7c38adefd0e785f91e871bd2d74c29e
Mariné du Plessis <mdp.marine@gmail.com>
2026-07-02 15:40:37 +0200

    Variables

---
+++ HelloWorld2/Variables/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var number = 7;
+            //var count = 10;
+            //var totalPrice = 20.95f;
+            //var character = 'B';
+            //var fisrtName = "Marine";
+            //var isWorking = true;
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(count);
+            //Console.WriteLine(totalPrice);
+            //Console.WriteLine(character);
+            //Console.WriteLine(fisrtName);
+            //Console.WriteLine(isWorking);
+
+            //const float Pi = 3.14f;
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+        }
+    }
+}
