e671bae1b48a8c85ac4c4ef68c9e8b8849f534fb
Mariné du Plessis <mdp.marine@gmail.com>
2026-07-03 20:05:56 +0200

    Classes

---
+++ CSharpFundamental/CSharpFundamental/Program.cs
@@ -0,0 +1,19 @@
+﻿using CSharpFundamental.Math;
+
+namespace CSharpFundamental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculater calculater = new Calculater();
+            var result = calculater.Add(1, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
