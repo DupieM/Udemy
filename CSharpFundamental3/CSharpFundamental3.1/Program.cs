6ef117efd2ce57d4ccb001496b8cb0d636f00f5a
Mariné du Plessis <mdp.marine@gmail.com>
2026-07-12 20:16:01 +0200

    Summarisiong Text

---
+++ CSharpFundamental3/CSharpFundamental3.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpFundamental3._1
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            // SUMMARISING TEXT
+            var sentence = "this is going to be a really really really really long text.";
+            var summary = StringUtility.SummarizeText(sentence);
+            Console.WriteLine(summary);
+        }
+    }
+}
---
+++ CSharpFundamental3/CSharpFundamental3.1/StringUtility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFundamental3._1
+{
+    public class StringUtility
+    {
+        public static string SummarizeText(string text, int maxLength = 20)
+        {
+
+            if (text.Length < maxLength)
+            {
+                return text;
+            }
+            else
+            {
+                var words = text.Split(' ');
+                var totalCharacter = 0;
+                var summaryWords = new List<string>();
+
+                foreach (var word in words)
+                {
+                    summaryWords.Add(word);
+
+                    totalCharacter += word.Length + 1;
+                    if (totalCharacter > maxLength)
+                        break;
+                }
+
+                return String.Join(" ", summaryWords) + "...";
+            }
+        }
+    }
+}
