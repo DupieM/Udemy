94b1886305399a97630e88ae98b0f6ce0435b103
Mariné du Plessis <mdp.marine@gmail.com>
2026-07-13 17:01:01 +0200

    Directory & DirectoryInfo

---
+++ CSharpFundamental3/CSharpFundamental3.4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace CSharpFundamental3._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // DIRECTORY & DIRECTORYINFO
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            //var folders = Directory.GetFiles(@"c:\Users\USER-PC\Documents\Udemy\Udemy\CSharpFundamental3", "*Program.cs", SearchOption.AllDirectories);
+            //foreach (var folder in folders)
+            //{
+            //    Console.WriteLine(folder);
+            //}
+                        
+            var directories = Directory.GetDirectories(@"c:\Users\USER-PC\Documents\Udemy\Udemy", "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+                Console.WriteLine(directory);
+
+            Directory.Exists("...");
+
+            var directoryInfo = new DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
--- CSharpFundamental3/CSharpFundamental3/obj/Debug/net10.0/CSharpFundamental3.AssemblyInfo.cs
+++ CSharpFundamental3/CSharpFundamental3/obj/Debug/net10.0/CSharpFundamental3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpFundamental3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e76cab0cc5a20e96bac36a6fb211d667551bba6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68da4836e80c3b994a624ae0a56451d396d6bbfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpFundamental3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpFundamental3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
